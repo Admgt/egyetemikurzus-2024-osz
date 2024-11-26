0eda8089404ee1042aeb149c6d6d3f7fa4267d04
Kalman Adam <h373742@stud.u-szeged.hu>
2024-11-26 16:33:18 +0100

    fajl kezeles

---
+++ ZO57BG/Konyvtar/ConsoleApp1/FileManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace LibraryManagementApp
+{
+    internal class FileManager
+    {
+        private readonly string _filePath;
+
+        public FileManager(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public List<Book> LoadBooks()
+        {
+            try
+            {
+                if (!File.Exists(_filePath))
+                {
+                    throw new FileNotFoundException("Nem talalhato a JSON fajl!");
+                }
+
+                string jsonContent = File.ReadAllText(_filePath);
+                return JsonSerializer.Deserialize<List<Book>>(jsonContent) ?? new List<Book>();
+            }
+            catch (JsonException ex)
+            {
+                Console.WriteLine($"Hiba: {ex.Message}");
+                return new List<Book>();
+            }
+            catch (Exception ex) {
+                Console.WriteLine($"Hiba: {ex.Message}");
+                return new List<Book>();
+            }
+        }
+    }
+}
