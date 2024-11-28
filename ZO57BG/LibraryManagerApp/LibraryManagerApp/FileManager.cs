beda8121d5cd4ff7d44a8598ba458fbd1d0558e8
Kalman Adam <h373742@stud.u-szeged.hu>
2024-11-28 17:03:57 +0100

    fajl kezelese

---
+++ ZO57BG/LibraryManagerApp/LibraryManagerApp/FileManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace LibraryManagerApp
+{
+    public class FileManager
+    {
+        private readonly string _filePath;
+
+        public FileManager(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public List<Book> LoadBooks()
+        {
+            try
+            {
+                if (!File.Exists(_filePath))
+                {
+                    throw new FileNotFoundException("Nem talalhato a JSON fajl");
+                }
+
+                string jsonContent = File.ReadAllText(_filePath);
+                return JsonSerializer.Deserialize<List<Book>>(jsonContent) ?? new List<Book>();
+            }
+            catch (JsonException ex)
+            {
+                Console.WriteLine($"Error parsing JSON data: {ex.Message}");
+                return new List<Book>();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected error: {ex.Message}");
+                return new List<Book>();
+            }
+        }
+    }
+}
